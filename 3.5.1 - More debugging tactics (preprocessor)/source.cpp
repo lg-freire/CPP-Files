5552f6efa9aa2608e3edc6548ddccd8ba8f8dbc4
lg-freire <l-gabriel@outlook.com>
2022-11-03 13:22:17 -0300

    3.5.1 - More debugging tactics (preprocessor)

---
+++ 3.5.1 - More debugging tactics (preprocessor)/source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// #define ENABLE_DEBUG // comment out to disable debugging statements
+
+int getUserInput() {
+#ifdef ENABLE_DEBUG
+	std::cerr << "getUserInput() called\n";
+#endif
+
+	std::cout << "Enter a number: ";
+	int x{};
+	std::cin >> x;
+
+	return x;
+}
+
+int main()
+{
+#ifdef ENABLE_DEBUG
+	std::cerr << "main() called\n";
+#endif
+
+	int x{ getUserInput() };
+	std::cout << "You entered: " << x << '\n';
+
+	return 0;
+}
